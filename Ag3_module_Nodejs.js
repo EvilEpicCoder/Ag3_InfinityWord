c09eb30fa1c7095f2df4bfbfa478f26e085159af
EvilEpicCoder <1st.elixile@gmail.com>
2017-11-18 10:06:59 +0300

    Node.js Base functionality working

--- Ag3_module_Nodejs.js
+++ Ag3_module_Nodejs.js
@@ -1,6 +1,6 @@
 //Module for server back-end
 const convert = require('./fileMain')
 var init=()=> {
-convert('/Wordlists_Base/ru_boys.list','/Wordlists_Base/file0011.txt',0,0,0);
+convert('/Wordlists_Base/ru_boys.list','/Wordlists_Base/file0011.txt','RU2EN','_',99);
 }
 init();
--- fileMain.js
+++ fileMain.js
@@ -5,10 +5,13 @@ const path = require('path');
 
 
 var convert=(inFile,outFile,direct,addChar,addNum)=>{
-const RU=['Й','Ц','У','К','Е','Н','Г','Ш','Щ','З','Х','Ъ','Ф','Ы','В','А','П','Р','О','Л','Д','Ж','Э' ,'/','|','Я','Ч','С','М','И','Т','Ь','Б','Ю',',' ,'й','ц','у','к','е','н','г','ш','щ','з','х','ъ','ф','ы','в','а','п','р','о','л','д','ж','э' ,'\\','/','я','ч','с','м','и','т','ь','б','ю','.'];
-const EN=['Q','W','E','R','T','Y','U','I','O','P','{','}','A','S','D','F','G','H','J','K','L',':','\"','|','>','Z','X','C','V','B','N','M','<','>','\?','q','w','e','r','t','y','u','i','o','p','[',']','a','s','d','f','g','h','j','k','l',';','\'','\\','<','z','x','c','v','b','n','m',',','.','/'];
+direct=direct.split('2');
+decode={
+  RU: ['Й','Ц','У','К','Е','Н','Г','Ш','Щ','З','Х','Ъ','Ф','Ы','В','А','П','Р','О','Л','Д','Ж','Э' ,'/','|','Я','Ч','С','М','И','Т','Ь','Б','Ю',',' ,'й','ц','у','к','е','н','г','ш','щ','з','х','ъ','ф','ы','в','а','п','р','о','л','д','ж','э' ,'\\','/','я','ч','с','м','и','т','ь','б','ю','.'],
+  EN: ['Q','W','E','R','T','Y','U','I','O','P','{','}','A','S','D','F','G','H','J','K','L',':','\"','|','>','Z','X','C','V','B','N','M','<','>','\?','q','w','e','r','t','y','u','i','o','p','[',']','a','s','d','f','g','h','j','k','l',';','\'','\\','<','z','x','c','v','b','n','m',',','.','/']
+}
 var linesNumber=0;
-
+console.log(direct);
   var readable = fs.createReadStream(__dirname+inFile,'utf8');
 
   var writeable = fs.createWriteStream(__dirname+outFile);
@@ -16,21 +19,32 @@ var linesNumber=0;
   readable.on('readable', function(chunk) {
     var buffer='';
     while (null !== (chunk = readable.read(1))) {
-      // if(chunk=='\n')
-      // console.log('newLINE!');
-      for(var a=0;a<RU.length;a++){
-        if(chunk==RU[a]){
-          chunk=EN[a];
+      for(var a=0;a<decode[direct[0]].length;a++){
+        if(chunk==decode[direct[0]][a]){
+          //console.log(K[direct[0]]);
+          //console.log(K[x]);
+          //RU Array = K['RU']
+          chunk=decode[direct[1]][a];
         }
       }
-      buffer+=chunk;
-      if(chunk=='\n'){
-        writeable.write(buffer);
-        buffer='';
-        linesNumber=counter(linesNumber);
-        console.log(linesNumber+' Lines converted');
-        //console.log(chunk);
+      if(chunk=='\n'){//if new line symbol in stream
+        if(addChar){
+          buffer+=addChar;
+        }//messy code add char at the end of line
+        if(addNum>0){
+          for(var g=0;g<addNum;g++){
+            buffer+=g;
+            linesNumber=counter(linesNumber);
+          }
+        }
+        buffer+='\n';
+        writeable.write(buffer);//collect data and write to file
+        buffer='';//clear buffer
+        linesNumber=counter(linesNumber);//update counter of the lines
+        console.log(linesNumber+' Lines converted');//inform user
+        //console.log(chunk);//shows current symbol
       }
+      buffer+=chunk;
       //console.log(chunk); // chunk is one symbol
     }
   });
